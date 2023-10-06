19019d445f85d8558a2890d26ae46ddf74d35f46
Łukasz Wieczorek <lukasz.wieczorek@stermedia.eu>
2023-10-06 17:04:42 +0200

    Add saints data extraction.

--- extractor/__main__.py
+++ extractor/__main__.py
@@ -1,17 +1,26 @@
 from pyquery import PyQuery as pq
-from lxml import etree
 
-parser = etree.HTMLParser()
+
+def extract_saints(d, position):
+    saints = []
+    selector = f"div.article__text > ul:nth-child({position})> li > a"
+    p = d(selector)
+    for a in p:
+        saints.append(a.text)
+    return saints
+
 
 def saints():
-    saints = []
-    tree = etree.parse("data/święci.html", parser)
-    return saints
+    d = pq(filename="data/święci.html")
+    female_saints = extract_saints(d, 12)
+    male_saints = extract_saints(d, 16)
+    return female_saints, male_saints
 
 
 def patrons():
     patrons = []
-    tree = etree.parse("data/patroni.html", parser)
+    d = pq(filename="data/patroni.html")
+    p = d("table")
     return patrons
 
 
@@ -22,13 +31,24 @@ def merged(saints_data, patrons_data):
 
 def main():
     print("Starting...")
+
     print("Saints...")
     saints_data = saints()
+    female_saints_data, male_saints_data = saints_data
+    print(female_saints_data)
+    print(male_saints_data)
+    print()
+
     print("Patrons...")
     patrons_data = patrons()
+    print(patrons_data)
+    print()
+
     print("Merged...")
     merged_data = merged(saints_data, patrons_data)
     print(merged_data)
+    print()
+
     print("Ending...")
 
 
